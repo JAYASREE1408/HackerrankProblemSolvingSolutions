7676d81c7bad87358d0ae26ecfbae2d44a275601
JAYASREE T <jayasreet2112@gmail.com>
2020-09-17 23:49:14 +0530

    Create Staircase.c

---
+++ Staircase.c
@@ -0,0 +1,22 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    int n,i,j,k;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=i;j<n;j++)
+        printf(" ");
+        for(k=1;k<=i;k++)
+        printf("#");
+    printf("\n");
+}
+}
