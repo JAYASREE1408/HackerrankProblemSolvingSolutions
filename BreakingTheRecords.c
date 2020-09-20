2a36e20d0255f7d129dab4db620ee558d00a2dde
JAYASREE T <jayasreet2112@gmail.com>
2020-09-21 00:54:52 +0530

    Breaking The Records

---
+++ BreakingTheRecords.c
@@ -0,0 +1,94 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+
+
+{
+
+
+    int n,i,a[1000],x=0,mn=0;
+
+
+    scanf("%d",&n);
+
+
+    for(i=0;i<n;i++)
+
+
+    {
+
+
+        scanf("%d",&a[i]);
+
+
+    }
+
+
+    int min=a[0],max=a[0];
+
+
+    for(i=0;i<n;i++)
+
+
+    {
+
+
+       if(min>a[i])
+
+
+       {
+
+
+           min=a[i];
+
+
+           mn++;
+
+
+       }
+
+
+    }
+
+
+    for(i=0;i<n;i++)
+
+
+    {
+
+
+        if(max<a[i])
+
+
+        {
+
+
+            max=a[i];
+
+
+            x++;
+
+
+        }
+
+
+    }
+
+
+    printf("%d %d",x,mn);
+
+
+    return 0;
+
+
+}
+
