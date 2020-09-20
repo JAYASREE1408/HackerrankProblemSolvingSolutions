1656a21dccacb8108f5d95b00272cd40201870c2
JAYASREE T <jayasreet2112@gmail.com>
2020-09-20 16:58:23 +0530

    Kangaroo

---
+++ Kangaroo.c
@@ -0,0 +1,28 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int k1,k2,x1,x2,v1,v2,i;
+    scanf("%d%d%d%d",&x1,&v1,&x2,&v2);
+    k1=x1;
+    k2=x2;
+      
+if(v1>v2)
+{
+    if((x2-x1)%(v1-v2) == 0)
+        printf("YES");
+    else
+        printf("NO");
+}
+else 
+    printf("NO");
+}
+   
