cd29a79eecf5bc3c7ad6c0237f46ee67c8ab9690
JAYASREE T <jayasreet2112@gmail.com>
2020-09-27 00:19:34 +0530

    Sub Array Division

---
+++ Sub-ArrayDivision.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    int i,n,a[n],m,d,count=0,sum=0;
+    scanf("%d\n",&n);
+    for(i=0;i<n;i++)
+    scanf("%d\n",&a[i]);
+    scanf("%d %d",&d,&m);
+    for(i=0;i<=n-1;i++)
+    
+    {
+      sum=0;
+        for(int j=0;j<m;j++)
+        {
+        sum=sum+a[i+j];
+        }
+       
+       if(sum==d) 
+       count=count+1;    
+       
+    }
+         printf("%d",count);
+        
+ 
+       
+}
+
+
