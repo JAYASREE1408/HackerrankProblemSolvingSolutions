307a818df75227eb7d52ff748c6218be4a0e3405
JAYASREE T <jayasreet2112@gmail.com>
2021-01-29 00:12:51 +0530

    Bill Division

---
+++ Bill Division.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int n,ar[100000];
+    int k,i,j;
+    int b;
+    int sum=0,diff=0,sum2=0;
+    scanf("%d ",&n);
+    scanf("%d\n",&b);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d ",&ar[i]);
+    }
+    scanf("%d",&k);
+
+    for(j=0;j<n;j++)
+    {
+        sum+=ar[j];
+    }
+    sum=sum-ar[b];
+    
+    sum2=sum/2;
+    diff=k-sum2;
+    if(sum2==k)
+    {
+      printf("Bon Appetit");
+    }
+    else
+    {
+         printf("%d",diff);
+       
+    }
+    
+    return 0;
+}
