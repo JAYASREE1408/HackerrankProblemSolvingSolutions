3db500a01f96ef28a5955e506c1b2cd3e95f97a4
JAYASREE T <jayasreet2112@gmail.com>
2021-03-22 17:30:38 +0530

    Delete a Node

---
+++ Delete a Node.c
@@ -0,0 +1,14 @@
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position) {
+    SinglyLinkedListNode *temp=head;
+    if(position==0){
+        head=head->next;
+        return head;
+    }
+    else{
+    for(int i=0;i<position-1;i++){
+        temp=temp->next;
+    }
+    temp->next=temp->next->next;
+    return head;
+}
+}
