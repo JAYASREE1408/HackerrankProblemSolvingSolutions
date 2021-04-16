281fe510b7aaed8efca3b68947be140a73c9ab8e
JAYASREE T <jayasreet2112@gmail.com>
2021-04-16 18:05:32 +0530

    Add files via upload

---
+++ BST insertion.c
@@ -0,0 +1,16 @@
+struct node* insert( struct node* root, int data ) {
+		if(root==NULL){
+            root=(struct node*)malloc(sizeof(struct node));
+            root->data=data;
+            root->left=NULL;
+            root->right=NULL;
+            return root;
+        }
+        if(data < root->data){
+            root->left=insert(root->left,data);
+        }
+        else{
+            root->right=insert(root->right,data);
+        }
+        return root;
+    }
